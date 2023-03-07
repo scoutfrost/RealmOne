5c518d669a681cedcf830936fc259ce4353e64c1
Magma!!! <117277406+Magma-FR@users.noreply.github.com>
2023-03-08 08:52:14 +1000

    Add files via upload

---
+++ RealmModMenu.cs
@@ -0,0 +1,46 @@
+﻿using Terraria;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria.ModLoader;
+using Terraria.Audio;
+using Terraria.ID;
+using RealmOne.Common;
+using RealmOne;
+using RealmOne.Projectiles;
+using RealmOne.Common.Systems;
+
+
+namespace RealmOne
+{
+    public class RealmModMenu : ModMenu
+    {
+        private const string menuAssetPath = "RealmOne/Assets/Textures/Menu"; // Creates a constant variable representing the texture path, so we don't have to write it out multiple times
+
+        public override Asset<Texture2D> Logo => ModContent.Request<Texture2D>("RealmOne/Assets/Textures/Menu/MenuLogo", (AssetRequestMode)2);
+
+        public override Asset<Texture2D> SunTexture => ModContent.Request<Texture2D>($"{menuAssetPath}/WormSun");
+
+        public override Asset<Texture2D> MoonTexture => ModContent.Request<Texture2D>($"{menuAssetPath}/WormMoon");
+
+        public override int Music => MusicLoader.GetMusicSlot(Mod, "Assets/Music/TheUnknown1");
+
+
+        public override ModSurfaceBackgroundStyle MenuBackgroundStyle => ModContent.GetInstance<SurfaceBack>();
+
+        public override string DisplayName => "The Last Paradox Aligns";
+
+        public override void OnSelected()
+        {
+            SoundEngine.PlaySound(rorAudio.ModMenuClick);
+
+        }
+
+        public override bool PreDrawLogo(SpriteBatch spriteBatch, ref Vector2 logoDrawCenter, ref float logoRotation, ref float logoScale, ref Color drawColor)
+        {
+            logoScale = 1.5f;
+            return true;
+            
+        }
+    }
+}
