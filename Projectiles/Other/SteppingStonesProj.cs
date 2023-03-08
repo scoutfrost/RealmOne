2731e45bcea630c498507661df397d5c269e2872
Magma!!! <117277406+Magma-FR@users.noreply.github.com>
2023-03-09 07:53:49 +1000

    Add files via upload

---
+++ Projectiles/Other/SteppingStonesProj.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.Audio;
+using System;
+
+namespace RealmOne.Projectiles.Other
+{
+
+    public class SteppingStonesProj : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Stepping Stones");
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 24;
+            Projectile.height = 10;
+
+            Projectile.aiStyle = ProjAIStyleID.IceRod;
+            Projectile.DamageType = DamageClass.Magic;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.ignoreWater = true;
+            Projectile.light = 0.6f;
+            Projectile.tileCollide = true;
+            Projectile.timeLeft = 80;
+            Projectile.penetrate = 3;
+            Projectile.CloneDefaults(ProjectileID.IceBlock);
+
+
+        }
+        public override void AI()
+        {
+            Projectile.aiStyle = ProjAIStyleID.IceRod;
+
+            Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, DustID.Stone, Projectile.velocity.X * 0.5f, Projectile.velocity.Y * 0.5f, Scale: 0.6f);
+            Lighting.AddLight(Projectile.position, 0.2f, 0.2f, 0.2f);
+            Lighting.Brightness(1, 1);
+
+
+        }
+        public override void Kill(int timeleft)
+        {
+            Collision.AnyCollision(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
+
+            for (var i = 0; i < 5; i++)
+            {
+                Vector2 speed = Main.rand.NextVector2CircularEdge(1f, 1f);
+                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.JungleGrass, 0f, 0f, 0, default, 2f);
+
+            }
+        }
+
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
