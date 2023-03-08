1d3dc37097b06726156386c0d63ebb1791f6d6fb
Magma!!! <117277406+Magma-FR@users.noreply.github.com>
2023-03-09 08:04:56 +1000

    Add files via upload

---
+++ Rarities/ModRarities.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using RealmOne.Rarities;
+
+namespace RealmOne.Rarities
+{
+	public class ModRarities: ModRarity
+	{
+		public override Color RarityColor => new Color(226, 220, 118);
+
+        public override int GetPrefixedRarity(int offset, float valueMult)
+		{
+			if (offset > 0)
+			{ // If the offset is 1 or 2 (a positive modifier).
+				return ModContent.RarityType<HigherTierModRarity>(); // Make the rarity of items that have this rarity with a positive modifier the higher tier one.
+			}
+
+			return Type; // no 'lower' tier to go to, so return the type of this rarity.
+		}
+	}
+
+    public class ModRarities1 : ModRarity
+    {
+        public override Color RarityColor => new Color(105, 160, 117);
+
+        public override int GetPrefixedRarity(int offset, float valueMult)
+        {
+            if (offset > 0)
+            { // If the offset is 1 or 2 (a positive modifier).
+                return ModContent.RarityType<PlantColor>(); // Make the rarity of items that have this rarity with a positive modifier the higher tier one.
+            }
+
+            return Type; // no 'lower' tier to go to, so return the type of this rarity.
+        }
+    }
+}
