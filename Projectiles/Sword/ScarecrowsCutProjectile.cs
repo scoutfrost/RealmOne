ded803f20eae91da9f10bc71280ca03f6a850db5
Magma!!! <117277406+Magma-FR@users.noreply.github.com>
2023-03-09 07:55:07 +1000

    Add files via upload

---
+++ Projectiles/Sword/ScarecrowsCutProjectile.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.Audio;
+using System;
+
+
+namespace RealmOne.Projectiles.Sword
+{
+
+    public class ScarecrowsCutProjectile : ModProjectile
+    {
+
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("cut");
+
+
+            Main.projFrames[Projectile.type] = 9;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 300;
+            Projectile.height = 200;
+
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.ignoreWater = true;
+            Projectile.light = 0.5f;
+            Projectile.tileCollide = false;
+            Projectile.timeLeft = 27;
+            Projectile.penetrate = -1;
+            Projectile.extraUpdates = 1;
+
+        }
+        public override void AI()
+        {
+            Projectile.aiStyle = 0;
+            Lighting.AddLight(Projectile.position, 0.2f, 0.2f, 0.2f);
+            Lighting.Brightness(1, 1);
+
+            if (++Projectile.frameCounter >= 3f)//the amount of ticks the game spends on each frame
+            {
+                Projectile.frameCounter = 0;
+
+                if (++Projectile.frame >= Main.projFrames[Projectile.type])
+                    Projectile.frame = 0;
+            }
+
+            Player player = Main.player[Projectile.owner];
+            Projectile.Center = player.Center;
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            Player p = Main.player[Projectile.owner];
+            int healingAmount = damage / 8; //decrease the value 30 to increase heal, increase value to decrease. Or you can just replace damage/x with a set value to heal, instead of making it based on damage.
+            p.statLife += healingAmount;
+            p.HealEffect(healingAmount, true);
+        }
+
+
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
---
+++ Projectiles/Sword/ShatteredGemBladeProj1.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using RealmOne.Common.Systems;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RealmOne.Projectiles.Sword
+{
+
+    public class ShatteredGemBladeProj1 : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Shattered Gem Blade Primary");
+            Main.projFrames[Projectile.type] = 4;
+
+            ProjectileID.Sets.CultistIsResistantTo[Projectile.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 130;
+            Projectile.height = 50;
+
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = false;
+            Projectile.timeLeft = 24;
+            Projectile.light = 1;
+            Projectile.penetrate = 2;
+            Projectile.extraUpdates = 1;
+            Projectile.scale = 1.5f;
+        }
+
+        public override void AI()
+        {
+            if (++Projectile.frameCounter >= 6f)//the amount of ticks the game spends on each frame
+            {
+                Projectile.frameCounter = 0;
+
+                if (++Projectile.frame >= Main.projFrames[Projectile.type])
+                    Projectile.frame = 0;
+            }
+            Projectile.aiStyle = 0;
+
+            Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, DustID.BlueCrystalShard, Projectile.velocity.X * 0.7f, Projectile.velocity.Y * 0.7f, Scale: 1f);   //spawns dust behind it, this is a spectral light blue dust
+            Player player = Main.player[Projectile.owner];
+            Vector2 ownerMountedCenter = player.RotatedRelativePoint(player.MountedCenter, true);
+
+            Projectile.direction = player.direction;
+            player.heldProj = Projectile.whoAmI;
+            Projectile.Center = ownerMountedCenter + new Vector2(40, 0) * player.direction;
+            player.ChangeDir(Projectile.direction);
+            Projectile.spriteDirection = Projectile.direction;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Vector2 drawPosition = Projectile.Center;
+            Color color = Color.Cyan;
+            _ = Lighting.GetColor((int)drawPosition.X / 16, (int)(drawPosition.Y / 16f));
+            SpriteEffects spriteEffects = SpriteEffects.None;
+            if (Projectile.spriteDirection == -1)
+                spriteEffects = SpriteEffects.FlipHorizontally;
+
+            Texture2D texture = (Texture2D)ModContent.Request<Texture2D>(Texture);
+
+            int frameHeight = texture.Height / Main.projFrames[Projectile.type];
+            int startY = frameHeight * Projectile.frame;
+
+            Rectangle sourceRectangle = new Rectangle(0, startY, texture.Width, frameHeight);
+
+            Vector2 origin = sourceRectangle.Size() / 2f;
+
+            float offsetX = 43;
+            origin.X = Projectile.spriteDirection == 1 ? sourceRectangle.Width - offsetX : offsetX;
+
+            //float offsetY = 40;
+            //origin.Y = (float)(Projectile.spriteDirection == 1 ? sourceRectangle.Height - offsetY : offsetY);
+
+            Color drawColor = Projectile.GetAlpha(lightColor);
+            Main.EntitySpriteDraw(texture,
+                Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY),
+                sourceRectangle, drawColor, Projectile.rotation, origin, Projectile.scale, spriteEffects, 0);
+
+            return false;
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            target.AddBuff(BuffID.WitheredArmor, 180);
+
+        }
+    }
+
+
+
+}
