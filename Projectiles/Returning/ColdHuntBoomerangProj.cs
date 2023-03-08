c01e718de50cc151c82c93d3900d8270d43b2394
Magma!!! <117277406+Magma-FR@users.noreply.github.com>
2023-03-09 07:54:33 +1000

    Add files via upload

---
+++ Projectiles/Returning/ColdHuntBoomerangProj.cs
@@ -0,0 +1,113 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+using Terraria.Audio;
+using Microsoft.Xna.Framework;
+using RealmOne.Common.Systems;
+
+namespace RealmOne.Projectiles.Returning
+{
+    public class ColdHuntBoomerangProj : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("ColdHunt Boomerang");
+
+            ProjectileID.Sets.CultistIsResistantTo[Projectile.type] = true;
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 10;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 28;
+            Projectile.height = 35;
+
+            Projectile.aiStyle = ProjAIStyleID.Boomerang;
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.ignoreWater = true;
+            Projectile.light = 0.4f;
+            Projectile.tileCollide = true;
+            Projectile.timeLeft = 600;
+            Projectile.extraUpdates = 1;
+
+            Projectile.penetrate = 1;
+            AIType = ProjectileID.IceBoomerang;
+        }
+
+        public override void AI()
+        {
+            int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Ice);
+            Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, DustID.IceTorch, Projectile.velocity.X * 0.5f, Projectile.velocity.Y * 0.5f, Scale: 0.5f);
+
+            Main.dust[dust].noGravity = true;
+
+            float maxDetectRadius = 300;
+            float projSpeed = 16;
+
+            NPC closestNPC = FindClosestNPC(maxDetectRadius);
+            if (closestNPC == null)
+                return;
+
+            Projectile.velocity = (closestNPC.Center - Projectile.Center).SafeNormalize(Vector2.Zero) * projSpeed;
+        }
+
+        public NPC FindClosestNPC(float maxDetectDistance)
+        {
+            NPC closestNPC = null;
+
+
+            float sqrMaxDetectDistance = maxDetectDistance * maxDetectDistance;
+
+
+            for (int k = 0; k < Main.maxNPCs; k++)
+            {
+                NPC target = Main.npc[k];
+
+                if (target.CanBeChasedBy())
+                {
+
+                    float sqrDistanceToTarget = Vector2.DistanceSquared(target.Center, Projectile.Center);
+
+
+                    if (sqrDistanceToTarget < sqrMaxDetectDistance)
+                    {
+                        sqrMaxDetectDistance = sqrDistanceToTarget;
+                        closestNPC = target;
+                    }
+                }
+            }
+
+            return closestNPC;
+        }
+        public override void Kill(int timeleft)
+        {
+
+            Collision.AnyCollision(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
+            SoundEngine.PlaySound(SoundID.Item39, Projectile.position);
+
+            for (var i = 0; i < 8; i++)
+                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Ice, 0f, 0f, 0, default, 1.5f);
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.velocity, ProjectileID.ThunderStaffShot, Projectile.damage, Projectile.knockBack, Main.myPlayer);
+            return true;
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+
+        {
+            SoundEngine.PlaySound(rorAudio.SFX_Shuriken, Projectile.position);
+            Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.velocity, ProjectileID.ThunderStaffShot, Projectile.damage, Projectile.knockBack, Main.myPlayer);
+
+            target.AddBuff(BuffID.Frostburn, 180);
+
+        }
+
+    }
+
+}
