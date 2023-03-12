27996b0eb3d95a899807911cc5ce1ba7e0e3fa5c
Magma!!! <117277406+Magma-FR@users.noreply.github.com>
2023-03-13 07:25:33 +1000

    Update DustStormBlunderbuss.cs

--- Items/Weapons/Ranged/DustStormBlunderbuss.cs
+++ Items/Weapons/Ranged/DustStormBlunderbuss.cs
@@ -34,10 +34,10 @@ namespace RealmOne.Items.Weapons.Ranged
             Item.DamageType = DamageClass.Ranged;
             Item.width = 32;
             Item.height = 32;
-            Item.useTime = 54;
-            Item.useAnimation = 54;
+            Item.useTime = 57;
+            Item.useAnimation = 57;
             Item.useStyle = 5;
-            Item.knockBack = 4f;
+            Item.knockBack = 2f;
             Item.value = 30000;
             Item.rare = 3;
             Item.UseSound = SoundID.DD2_ExplosiveTrapExplode;
