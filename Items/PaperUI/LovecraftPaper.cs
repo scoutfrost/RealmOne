4f5d7f80dca73f211438f83ce9d3ade80b897437
Magma!!! <117277406+Magma-FR@users.noreply.github.com>
2023-05-12 21:32:54 +1000

    Add files via upload

---
+++ Items/PaperUI/LovecraftPaper.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+using System.ComponentModel.DataAnnotations;
+using RealmOne.Items.Misc;
+using RealmOne.Rarities;
+using RealmOne.RealmPlayer;
+using RealmOne.Common.Systems;
+
+namespace RealmOne.Items.PaperUi
+{
+    public class LovecraftPaper : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("The World we live in - H.P Lovecraft"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+
+            Tooltip.SetDefault("Open a scroll of Lovecraft's Ancient Scripts.");
+
+
+
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+            Item.rare = ModContent.RarityType<ModRarities>();
+            Item.maxStack = 1;
+            Item.UseSound = rorAudio.SFX_Scroll;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.useAnimation = 1;
+            Item.useTime = 1;
+            Item.autoReuse = false;
+            Item.reuseDelay = 29;
+            Item.noUseGraphic = true;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<Parchment>(), 3)
+                                .AddIngredient(ModContent.ItemType<EidolicInk>(), 3)
+
+                .AddTile(TileID.WorkBenches)
+                .Register();
+        }
+
+        public override bool? UseItem(Player player)
+        {
+            if (player.GetModPlayer<Scrolly>().ShowScroll == false)
+            {
+                player.GetModPlayer<Scrolly>().ShowScroll = true;
+            }
+            else
+            {
+                player.GetModPlayer<Scrolly>().ShowScroll = false;
+            }
+
+            return base.UseItem(player);
+        }
+
+
+
+    }
+}
