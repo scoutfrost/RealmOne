54c57ebace8ec66e9d548c97451aef492902178c
Magma!!! <117277406+Magma-FR@users.noreply.github.com>
2023-05-12 20:11:09 +1000

    Add files via upload

---
+++ Buffs/Debuffs/SharpHeat.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+
+namespace RealmOne.Buffs.Debuffs
+{
+    public class SharpHeat : ModBuff
+    {
+        public override string Texture => "RealmOne/Buffs/HellfireVodkaBuff";
+
+        private readonly Color Blue = new Color(0, 114, 201);
+        private readonly Color Orange = new Color(255, 194, 89);
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Sharp Heat");
+            Description.SetDefault("Burns the enemy with sharp heat, burns faster if their health is under 4");
+
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = true;
+            Main.pvpBuff[Type] = true;
+            Main.buffNoSave[Type] = true;
+            Terraria.ID.BuffID.Sets.LongerExpertDebuff[Type] = true;
+        }
+
+
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+
+            if (Main.rand.NextBool(2))
+            {
+                int dust = Dust.NewDust(npc.position, npc.width, npc.height, DustID.GoldFlame);
+                Main.dust[dust].velocity.X *= 0.1f;
+                Main.dust[dust].velocity.Y *= 0.4f;
+            }
+            if (!npc.friendly)
+            {
+                if (npc.defense < 5)
+                    npc.lifeRegen -= 12;
+
+                
+                else
+                    npc.lifeRegen -= 4;
+
+                
+            }
+        }
+    }
+}
---
+++ Buffs/Debuffs/Splintered.cs
@@ -0,0 +1,37 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using RealmOne.Buffs;
+using RealmOne.Items;
+
+namespace RealmOne.Buffs.Debuffs
+{
+    public class Splintered : ModBuff
+    {
+        public override void SetStaticDefaults()
+
+
+        {
+            DisplayName.SetDefault("Splinted");
+            Description.SetDefault("'Ouch!'");
+            Main.buffNoSave[Type] = true;
+            Main.debuff[Type] = true;
+            
+
+        }
+
+
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+            npc.life -= 2;
+            if (npc.life <= 0)
+            {
+                npc.life = 1; // Make sure the NPC's life is never less than 1
+                npc.checkDead(); // Kill the NPC if its life is less than or equal to 0
+            }
+        }
+
+    }
+
+
+}
