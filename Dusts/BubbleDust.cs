1e86ce9b7bbdf88906a286edf0a3fac43040360c
Magma!!! <117277406+Magma-FR@users.noreply.github.com>
2023-05-12 21:08:16 +1000

    Add files via upload

---
+++ Dusts/BubbleDust.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace RealmOne.Dusts
+{
+    public class BubbleDust : ModDust
+    {
+        public override void OnSpawn(Dust dust)
+        {
+            dust.noGravity = true;
+            dust.frame = new Rectangle(0, 0, 30, 30);
+            // If our texture had 3 different dust on top of each other (a 30x90 pixel image), we might do this:
+            // dust.frame = new Rectangle(0, Main.rand.Next(3) * 30, 30, 30);
+        }
+
+        public override bool Update(Dust dust)
+        {
+            // Move the dust based on its velocity and reduce its size to then remove it, as the 'return false;' at the end will prevent vanilla logic.
+            dust.position += dust.velocity;
+            
+            dust.scale -= 0.01f;
+            float light = 0.35f * dust.scale;
+
+            Lighting.AddLight(dust.position, light, light, light);
+            if (dust.scale < 0.75f)
+                dust.active = false;
+
+            return false;
+        }
+    }
+}
