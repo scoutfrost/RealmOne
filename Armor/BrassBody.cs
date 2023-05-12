a902b9794c60ea977106963297e696e1df3018ca
Magma!!! <117277406+Magma-FR@users.noreply.github.com>
2023-05-12 20:02:28 +1000

    Add files via upload

---
+++ Armor/BrassBody.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RealmOne;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+using RealmOne.Projectiles;
+using Terraria.Localization;
+using Terraria.Audio;
+using RealmOne.Common.Systems;
+using RealmOne.Items.Ammo;
+using RealmOne.Items;
+using RealmOne.Armor;
+
+namespace RealmOne.Armor
+{
+    [AutoloadEquip(EquipType.Body)]
+
+    public class BrassBody : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            DisplayName.SetDefault("Brass Chestplate");
+            Tooltip.SetDefault("8% knockback and weapon speed"
+                +"\n'A knight in rusty armour!'");
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+
+        }
+        public override void SetDefaults()
+        { 
+            Item.width = 18; // Width of the item
+            Item.height = 18; // Height of the item
+            Item.value = Item.sellPrice(gold: 1); // How many coins the item is worth
+            Item.rare = ItemRarityID.Blue; // The rarity of the item
+            Item.defense = 5; // The amount of defense the item will give when equipped
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+
+            player.GetAttackSpeed(DamageClass.Generic) += 0.08f;
+            player.GetKnockback(DamageClass.Generic) += 0.08f;
+        }
+
+        // IsArmorSet determines what armor pieces are needed for the setbonus to take effect
+
+
+        // UpdateArmorSet allows you to give set bonuses to the armor.
+
+
+
+    }
+}
---
+++ Armor/BrassLegs.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RealmOne;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+using RealmOne.Projectiles;
+using Terraria.Localization;
+using Terraria.Audio;
+using RealmOne.Common.Systems;
+using RealmOne.Items.Ammo;
+using RealmOne.Items;
+using RealmOne.Armor;
+
+
+namespace RealmOne.Armor
+{
+    [AutoloadEquip(EquipType.Legs)]
+
+    public class BrassLegs: ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+           
+            DisplayName.SetDefault("Brass Greaves");
+            Tooltip.SetDefault("7% increased running and movement speed");
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+
+            // If your head equipment should draw hair while drawn, use one of the following:
+            // ArmorIDs.Head.Sets.DrawHead[Item.headSlot] = false; // Don't draw the head at all. Used by Space Creature Mask
+            // ArmorIDs.Head.Sets.DrawHatHair[Item.headSlot] = true; // Draw hair as if a hat was covering the top. Used by Wizards Hat
+            // ArmorIDs.Head.Sets.DrawFullHair[Item.headSlot] = true; // Draw all hair as normal. Used by Mime Mask, Sunglasses
+            // ArmorIDs.Head.Sets.DrawBackHair[Item.headSlot] = true;
+            // ArmorIDs.Head.Sets.DrawsBackHairWithoutHeadgear[Item.headSlot] = true; 
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 18; // Width of the item
+            Item.height = 18; // Height of the item
+            Item.value = Item.sellPrice(gold: 1); // How many coins the item is worth
+            Item.rare = ItemRarityID.Green; // The rarity of the item
+            Item.defense = 3; // The amount of defense the item will give when equipped
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.maxRunSpeed += 0.07f;
+            player.moveSpeed += 0.07f;
+
+        }
+
+        // IsArmorSet determines what armor pieces are needed for the setbonus to take effect
+
+
+        // UpdateArmorSet allows you to give set bonuses to the armor.
+
+
+        // Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+       
+    }
+}
