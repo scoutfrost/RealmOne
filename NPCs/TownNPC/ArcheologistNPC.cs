2b7e85827ece279fcd0ff326fd85b868aa7f2f7e
Magma!!! <117277406+Magma-FR@users.noreply.github.com>
2023-05-13 01:05:26 +1000

    Add files via upload

---
+++ NPCs/TownNPC/ArcheologistNPC.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.GameContent.Personalities;
+using Terraria.ID;
+using Terraria.Localization;
+using static Terraria.ModLoader.ModContent;
+using Terraria.GameContent.Bestiary;
+using RealmOne.Items.Misc;
+using RealmOne.Projectiles.Throwing;
+using RealmOne.Items.Placeables;
+using RealmOne.Common.Systems;
+using RealmOne.Items.Weapons.PreHM.Throwing;
+
+namespace RealmOne.NPCs.TownNPC
+{
+    [AutoloadHead]
+    public class ArcheologistNPC : ModNPC
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("The Archeologist.");
+            Main.npcFrameCount[NPC.type] = 26;
+            NPCID.Sets.ExtraFramesCount[NPC.type] = 9;
+            NPCID.Sets.AttackFrameCount[NPC.type] = 4;
+            NPCID.Sets.DangerDetectRange[NPC.type] = 1500;
+            NPCID.Sets.AttackType[NPC.type] = 0;
+            NPCID.Sets.AttackTime[NPC.type] = 20;
+            NPCID.Sets.AttackAverageChance[NPC.type] = 10;
+
+            NPC.Happiness
+                .SetBiomeAffection<JungleBiome>(AffectionLevel.Like)
+                .SetBiomeAffection<SnowBiome>(AffectionLevel.Hate)
+                .SetBiomeAffection<ForestBiome>(AffectionLevel.Love)
+                
+                .SetNPCAffection(NPCID.Merchant, AffectionLevel.Love)
+                .SetNPCAffection(NPCID.PartyGirl, AffectionLevel.Dislike)
+                .SetNPCAffection(NPCID.Mechanic, AffectionLevel.Hate);
+        }
+
+
+        public override void SetDefaults()
+        {
+            NPC.townNPC = true;
+            NPC.friendly = true;
+            NPC.aiStyle = 7;
+            NPC.damage = 20;
+            NPC.defense = 30;
+            NPC.lifeMax = 300;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            NPC.knockBackResist = 0.4f;
+            AnimationType = NPCID.Guide;
+        }
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        {
+            bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[] {
+                BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.Surface,
+                new FlavorTextBestiaryInfoElement("An experienced and wise explorer, writing a plethora of notes of different phenomena around the land. Will give any confident and adept traveller a mission to complete his dire requests and miscellanous."),
+            });
+        }
+
+        public override bool CanTownNPCSpawn(int numTownNPCs, int money)
+        {
+            if (NPC.AnyNPCs(NPCType<BoundArcheologist>()))
+                return false;
+
+            for (int r = 0; r < 255; r++)
+            {
+                Player player = Main.player[r];
+                if (player.active)
+                    for (int j = 0; j < player.inventory.Length; j++)
+                        if (player.inventory[j].type == ItemID.PlatinumCoin)
+                            return true;
+            }
+            return false;
+        }
+
+        public override List<string> SetNPCNameList()
+            => new() { "Frederick", "Marvin", "Charles", "Arthur", "George", "Albert", "Dens" };
+
+        public override string GetChat()
+        {
+            var chatt = new List<string>
+            {
+                "My hand will never get tired of writing, even with frostbite!",
+                "Exploring and researching is therapeutic",
+                "Embrace nature, reject violence",
+                "I could take on that beast again if I had the chance!",
+                "I used to smoke, but I've kept all my cigarettes in my stash",
+                "I sense great determination and morality in you"
+            };
+            return Main.rand.Next(chatt);
+        }
+        public override void SetChatButtons(ref string button, ref string button2)
+        {
+            button = "Shop";
+            button2 = "Prospector Missions";
+        }
+
+        public override void OnChatButtonClicked(bool firstButton, ref bool shop)
+        {
+            if (firstButton)
+                shop = true;
+        }
+
+        public override void SetupShop(Chest shop, ref int nextSlot)
+        {
+
+            shop.item[nextSlot].SetDefaults(ItemID.FrostburnArrow, false);
+            shop.item[nextSlot].value = Item.buyPrice(copper:7);
+            nextSlot++;
+
+            shop.item[nextSlot].SetDefaults(ItemID.Snowball, false);
+            shop.item[nextSlot].value = Item.buyPrice(copper: 2);
+            nextSlot++;
+
+            shop.item[nextSlot].SetDefaults(ItemID.SnowballLauncher, false);
+            shop.item[nextSlot].value = Item.buyPrice(gold: 3, silver: 40) ;
+            nextSlot++;                                                                                                     
+
+            shop.item[nextSlot].SetDefaults(ItemID.SnowballCannon, false);
+            shop.item[nextSlot].value = Item.buyPrice(gold: 8, silver: 20);
+            nextSlot++;
+
+            shop.item[nextSlot].SetDefaults(ItemID.Rope, false);
+            shop.item[nextSlot].value = Item.buyPrice(copper: 70);
+            nextSlot++;
+
+            shop.item[nextSlot].SetDefaults(ItemID.HunterPotion, false);
+            shop.item[nextSlot].value = Item.buyPrice(gold: 2, silver: 50);
+            nextSlot++;
+
+
+            shop.item[nextSlot].SetDefaults(ItemID.TrapsightPotion, false);
+            shop.item[nextSlot].value = Item.buyPrice(gold: 2, silver: 20);
+            nextSlot++;
+
+            shop.item[nextSlot].SetDefaults(ItemID.WarmthPotion, false);
+            shop.item[nextSlot].value = Item.buyPrice(gold: 2);
+            nextSlot++;
+
+
+            shop.item[nextSlot].SetDefaults(ItemID.TeaKettle, false);
+            shop.item[nextSlot].value = Item.buyPrice(gold: 3);
+            nextSlot++;
+
+            shop.item[nextSlot].SetDefaults(ItemID.Teacup, false);
+            shop.item[nextSlot].value = Item.buyPrice(gold: 1, silver: 50);
+            nextSlot++;
+
+            shop.item[nextSlot].SetDefaults(ItemID.CoffeeCup, false);
+            shop.item[nextSlot].value = Item.buyPrice(gold: 3);
+            nextSlot++;
+
+            shop.item[nextSlot].SetDefaults(ItemID.IceTorch, false);
+            shop.item[nextSlot].value = Item.buyPrice(copper: 50);
+            nextSlot++;
+
+            shop.item[nextSlot].SetDefaults(ItemID.Campfire, false);
+            shop.item[nextSlot].value = Item.buyPrice(silver: 90);
+            nextSlot++;
+
+            shop.item[nextSlot].SetDefaults(ItemType<StoneOvenItem>(), false);
+            shop.item[nextSlot].value = Item.buyPrice(gold: 3, silver: 22);
+            nextSlot++;
+
+            shop.item[nextSlot].SetDefaults(ModContent.ItemType<Parchment>(), false);
+            shop.item[nextSlot].value = Item.buyPrice(silver: 80);
+            nextSlot++;
+
+            shop.item[nextSlot].SetDefaults(ItemID.Extractinator, false);
+            shop.item[nextSlot].value = Item.buyPrice(gold: 3);
+            nextSlot++;
+
+            shop.item[nextSlot].SetDefaults(ItemID.ChristmasTree, false);
+            shop.item[nextSlot].value = Item.buyPrice(gold: 1);
+            nextSlot++;
+
+
+            if (DownedBossSystem.downedSquirmo)
+            {
+                shop.item[nextSlot].SetDefaults(ItemID.PlatinumBow, false);
+                nextSlot++;
+            }
+        }
+
+        public override void TownNPCAttackStrength(ref int damage, ref float knockback)
+        {
+            damage = 10;
+            knockback = 3f;
+        }
+
+        public override void TownNPCAttackCooldown(ref int cooldown, ref int randExtraCooldown)
+        {
+            cooldown = 15;
+            randExtraCooldown = 20;
+        }
+
+        public override void TownNPCAttackProj(ref int projType, ref int attackDelay)
+        {
+            projType = ProjectileType<TundraThrowingKnifeProjectile>();
+            attackDelay = 15;
+        }
+
+        public override void TownNPCAttackProjSpeed(ref float multiplier, ref float gravityCorrection, ref float randomOffset)
+        {
+            multiplier = 15f;
+        }
+        public override void OnKill()
+        {
+            Item.NewItem(NPC.GetSource_Death(), NPC.getRect(), ItemType<TundraThrowingKnife>(), 50, false, 0, false, false);
+        }
+    }
+}
---
+++ NPCs/TownNPC/BoundArcheologist.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using static Terraria.ModLoader.ModContent;
+using RealmOne.Items.Misc;
+using RealmOne.Common.Systems;
+using Terraria.ModLoader.Utilities;
+
+namespace RealmOne.NPCs.TownNPC
+{
+    public class BoundArcheologist : ModNPC
+    {
+
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Snowbanked Archeologist");
+            NPCID.Sets.TownCritter[NPC.type] = true;
+
+            NPCID.Sets.NPCBestiaryDrawModifiers drawModifiers = new NPCID.Sets.NPCBestiaryDrawModifiers(0)
+            {
+                Hide = true
+            };
+            NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, drawModifiers);
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.friendly = true;
+            NPC.townNPC = true;
+            NPC.dontTakeDamage = true;
+            NPC.width = 32;
+            NPC.height = 48;
+            NPC.aiStyle = 0;
+            NPC.damage = 0;
+            NPC.defense = 20;
+            NPC.lifeMax = 10000;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            NPC.knockBackResist = 0f;
+            NPC.rarity = 1;
+        }
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            //If any player is underground and has an example item in their inventory, the example bone merchant will have a slight chance to spawn.
+            if (spawnInfo.Player.ZoneSnow && DownedBossSystem.downedSquirmo)
+            {
+                return 0.09f;
+            }
+            
+            //Else, the example bone merchant will not spawn if the above conditions are not met.
+            return 0f;
+        }
+
+        public override bool? DrawHealthBar(byte hbPosition, ref float scale, ref Vector2 position) => false;
+
+        public override string GetChat() => "Dear God, I thought I was gonna get permanent frostbite if you didn't come to my rescue! Thank you kind lad, now excuse me but do you a kettle and tea? That would surely warm the old and frigid hands of mine right up! ";
+
+        public override void AI()
+        {
+            if (Main.netMode != NetmodeID.MultiplayerClient)
+            {
+                NPC.homeless = false;
+                NPC.homeTileX = -1;
+                NPC.homeTileY = -1;
+                NPC.netUpdate = true;
+            }
+
+            if (NPC.wet)
+                NPC.life = 250;
+
+            foreach (var player in Main.player)
+            {
+                if (!player.active)
+                    continue;
+
+                if (player.talkNPC == NPC.whoAmI)
+                {
+                    Rescue();
+                    return;
+                }
+            }
+        }
+
+        public void Rescue()
+        {
+            NPC.Transform(NPCType<ArcheologistNPC>());
+            NPC.dontTakeDamage = false;
+        }
+    }
+}
