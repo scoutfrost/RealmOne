3853d31188dfc11cb5e844528046dc460e8dc3de
scoutfrost <125180429+scoutfrost@users.noreply.github.com>
2023-08-13 02:01:19 -0700

    yippee made barn naturally spawn

--- Biomes/GenPasses/AbandondedFarmGen.cs
+++ Biomes/GenPasses/AbandondedFarmGen.cs
@@ -1,98 +1,103 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
-using RealmOne.Tiles;
-using RealmOne.Tiles.Blocks;
-using StructureHelper;
-using System.Collections.Generic;
 using Terraria;
-using Terraria.DataStructures;
-using Terraria.GameContent.Generation;
-using Terraria.ID;
-using Terraria.IO;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Input;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.GameContent.Generation;
+using System.Collections.Generic;
 using Terraria.WorldBuilding;
+using Terraria.IO;
+using System;
+using RealmOne.Tiles.Blocks;
+using RealmOne.Tiles;
+using StructureHelper;
+using Terraria.DataStructures;
+using RealmOne;
+using System.Reflection.Emit;
+
 namespace RealmOne.Biomes.GenPasses
 {
-    public class AbandondedFarmGen : ModSystem
-    {
+	public class AbandondedFarmGen : ModSystem
+	{
         public override void ModifyWorldGenTasks(List<GenPass> tasks, ref double totalWeight)
-        {
-            int iceBiomeGenIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Floating Islands"));
-            if (iceBiomeGenIndex != -1)
-                tasks.Insert(iceBiomeGenIndex - 1, new PassLegacy("Abandoned Farm", FarmBiomeGen));
-        }
-        bool validLocation = false;
+		{
+			int genIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Living Trees"));
+			if (genIndex != -1)
+				tasks.Insert(genIndex - 1, new PassLegacy("Abandoned Farm", FarmBiomeGen));
+		}
+		int startX = (Main.maxTilesX / 2) + WorldGen.genRand.Next(100, 150);
         public void FarmBiomeGen(GenerationProgress progress, GameConfiguration config)
-        {
-            //bool right = WorldGen.genRand.NextBool();
-            int startX = (Main.maxTilesX / 2) + WorldGen.genRand.Next(100, 150) + (int)(Main.maxTilesX / Main.maxTilesY);
-            int endX = startX + WorldGen.genRand.Next(175, 225) + (int)(Main.maxTilesX / Main.maxTilesY);
+		{
+            int endX = startX + WorldGen.genRand.Next(200, 250) + Main.maxTilesY / 200;
             int attempts = 0;
+            bool validLocation = false;
             for (int i = startX; i < endX; i++)
-            {
-                int y = 0;
-                while (y < Main.worldSurface)
-                {
-                    if (WorldGen.SolidTile(i, y))
-                    {
-                        if (i == startX + (endX - startX) / 2)
-                        {
-                            Generator.GenerateStructure("Structures/Barn1", new Point16(i, y), Mod, false);
-                        }
-                        if (i == startX)
-                        {
-                            Dictionary<ushort, int> dictionary = new Dictionary<ushort, int>();
-                            WorldUtils.Gen(new Point(startX, y + 15), new Shapes.Rectangle(endX, 30), new Actions.TileScanner(TileID.Dirt).Output(dictionary));
-                            int dirtCount = dictionary[TileID.Dirt];
+			{
+				int y = (int)(Main.worldSurface * 0.35f);
+				while (y < Main.worldSurface)
+				{
+					if (WorldGen.SolidTile(i, y))
+					{
+						if (i == startX)
+						{
+							Dictionary<ushort, int> dictionary = new Dictionary<ushort, int>();
+							WorldUtils.Gen(new Point(startX, y + 15), new Shapes.Rectangle(endX, 30), new Actions.TileScanner(TileID.Dirt).Output(dictionary));
+							int dirtCount = dictionary[TileID.Dirt];
 
-                            if (dirtCount > endX * 30 / 4)
-                            {
-                                validLocation = true;
-                            }
-                        }
-                        if (validLocation || attempts >= 30)
-                        {
-                            WorldGen.EmptyLiquid(i, y);
-                            WorldGen.TileRunner(i, y, WorldGen.genRand.Next(35, 45), WorldGen.genRand.Next(10, 15), ModContent.TileType<FarmSoil>());
+							if (dirtCount > endX * 30 / 4)
+							{
+								validLocation = true;
+							}
+						}
+						if (validLocation || attempts >= 30)
+						{
+							WorldGen.EmptyLiquid(i, y);
+							WorldGen.TileRunner(i, y, WorldGen.genRand.Next(35, 45), WorldGen.genRand.Next(10, 15), ModContent.TileType<FarmSoil>());
+						}
+						else
+						{
+      						attempts++;
+							endX -= 1;
+							startX -= 1;
+							i = startX;
+						}
+						break;
+					}
+					y++;
+				}
+			}
+		}
 
-                            if (i == startX)
-                            {
-                                WorldGen.PlaceObject(i, y, ModContent.TileType<TractorTile>(), true);
-                            }
-
-                            int rand = WorldGen.genRand.Next(1, 10);
-                            if (rand == 1)
-                            {
-                                WorldGen.PlaceObject(i, y, ModContent.TileType<TractorTile>(), true);
-                            }
-                        }
-                        else
-                        {
-                            attempts++;
-                            endX -= 1;
-                            startX -= 1;
-                            i = startX;
-                        }
-                        break;
-                    }
-                    y++;
-                }
-            }
-        }
-        public static bool JustPressed(Keys key)
+        public override void PostWorldGen()
         {
-            return Main.keyState.IsKeyDown(key) && !Main.oldKeyState.IsKeyDown(key);
+			int barnStartX = startX + WorldGen.genRand.Next(10, 25);
+			int y = 0;
+			bool validLocation = false;
+			int barnWidth = 83;
+			int barnHeight = 41;
+			while (y < Main.worldSurface)
+			{
+				y++;
+				if (WorldGen.SolidTile(barnStartX, y) && Framing.GetTileSafely(barnStartX, y).TileType == ModContent.TileType<FarmSoil>())
+				{
+					validLocation = true;
+					break;
+				}
+			}
+			if (validLocation)
+			{
+				Generator.GenerateStructure("Structures/Barn1", new Point16(barnStartX, y - barnHeight), Mod, false);
+				for (int i = barnStartX; i <= barnWidth; i++)
+				{
+					int j = y + barnHeight;
+					while(!WorldGen.SolidTile(i, j))
+					{
+						j--;
+						WorldGen.PlaceTile(i, j, ModContent.TileType<FarmSoil>());
+					}
+				}
+			}
         }
-        public override void PostUpdateWorld()
-        {
-            if (JustPressed(Keys.D1))
-                TestMethod((int)Main.MouseWorld.X / 16, (int)Main.MouseWorld.Y / 16);
-        }
-
-        private void TestMethod(int x, int y1)
-        {
-            Dust.QuickBox(new Vector2(x, y1) * 16, new Vector2(x + 1, y1 + 1) * 16, 2, Color.YellowGreen, null);
-            WorldGen.PlaceObject(x, y1, ModContent.TileType<TractorTile>(), true);
-        }
-    }
+	}
 }
+
