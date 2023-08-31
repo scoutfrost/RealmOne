8b75d73117ad2bc4731cfcb7251b3493052ceea6
Mr-Liquid-Sure <141588242+Mr-Liquid-Sure@users.noreply.github.com>
2023-08-31 16:37:29 +0100

    Tattered Javelin added

---
+++ Items/Sets/TatteredWoodSet/TatteredJavelin.cs
@@ -0,0 +1,50 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+using System;
+using RealmOne.Items.Placeables;
+using RealmOne.Items.Placeables.FarmStuff;
+using RealmOne.Projectiles.Throwing;
+
+
+namespace RealmOne.Items.Sets.TatteredWoodSet
+{
+    public class TatteredJavlin : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.shoot = ModContent.ProjectileType<TatteredJavlinProj>();
+            Item.DamageType = DamageClass.Ranged;
+            Item.damage = 14;
+            Item.width = 24;
+            Item.height = 24;
+            Item.useTime = 22;
+            Item.useAnimation = 22;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 1f;
+            Item.value = 10000;
+            Item.rare = ItemRarityID.Blue;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = false;
+            Item.maxStack = 9999;
+            Item.consumable = true;
+            Item.shootSpeed = 9f;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+
+
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<TatteredWood>(), 10)
+                .AddTile(TileID.WorkBenches)
+                .Register();
+        }
+  
+
+    }
+}
