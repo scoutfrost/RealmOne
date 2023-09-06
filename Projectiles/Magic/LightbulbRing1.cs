b7eeb9536dc1d08687b71c4b825b86bf0e969c5f
scoutfrost <125180429+scoutfrost@users.noreply.github.com>
2023-09-06 00:19:32 -0700

    twigbulb
    yeeeeppp

--- Projectiles/Magic/LightbulbRing1.cs
+++ Projectiles/Magic/LightbulbRing1.cs
@@ -1,76 +1,123 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using ReLogic.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Terraria;
-using Terraria.ID;
 using Terraria.ModLoader;
-using static Terraria.ModLoader.ModContent;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.DataStructures;
+using ReLogic.Content;
+using Terraria.GameContent;
+using RealmOne.Common.Core;
 
 namespace RealmOne.Projectiles.Magic
-{
-
-    public class LightBulbRing1 : ModProjectile
+{ 
+    internal class LightBulbRing1 : ModProjectile
     {
-        private static Asset<Texture2D> LightbulbRing;
-
+        static Texture2D tex;
+        public override void Load()
+        {
+            tex = (Texture2D)ModContent.Request<Texture2D>("RealmOne/Assets/Effects/LightbulbShine", AssetRequestMode.ImmediateLoad);
+        }
+        public override void Unload()
+        {
+            tex = null;
+        }
+        public override string Texture => Helper.Empty;
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Lightbulb Ring");
-
-            ProjectileID.Sets.CultistIsResistantTo[Projectile.type] = true;
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 5;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
         }
-
         public override void SetDefaults()
         {
-            Projectile.width = 260;
-            Projectile.height = 260;
-
-            Projectile.aiStyle = 9;
-            Projectile.DamageType = DamageClass.Magic;
+            Projectile.width = 30;
+            Projectile.height = 30;
             Projectile.friendly = true;
             Projectile.hostile = false;
-            Projectile.ignoreWater = true;
-            Projectile.light = 3.5f;
+            Projectile.DamageType = DamageClass.Magic;
             Projectile.tileCollide = false;
-            Projectile.timeLeft = 100;
-            Projectile.netImportant = true;
-            Projectile.netUpdate = true;
-            Projectile.penetrate = 5;
-            Projectile.stepSpeed = 1f;
-            Projectile.alpha = 255;
-            Projectile.scale = 1f;
-
+            Projectile.penetrate = -1;
+            Projectile.damage = 1;
         }
-
-        public override void AI() // this may add flamelash
+        ref float Timer => ref Projectile.ai[0];
+        ref float NumOfImages => ref Projectile.ai[1];
+        Vector2 scale;
+        bool channel = false;   
+        public override void OnSpawn(IEntitySource source)
         {
-            //   AIType = ProjectileID.Flamelash;
-            Lighting.AddLight(Projectile.position, 3f, 3f, 1f);
-            Lighting.Brightness(2, 2);
-
-            //  Projectile.localAI[0] += 1f;
+            NumOfImages = 1;
         }
+        public override void AI()
+        {
+            Player player = Main.player[Projectile.owner];
+            int maxNumImages = 8;
 
-        public override void Load()
-        { // This is called once on mod (re)load when this piece of content is being loaded.
-          // This is the path to the texture that we'll use for the hook's chain. Make sure to update it.
-            LightbulbRing = Request<Texture2D>("RealmOne/Assets/Effects/LightbulbShine");
-        }
+            player.itemAnimation = 2;
+            player.itemTime = 2;
 
-        public override void Unload()
-        { // This is called once on mod reload when this piece of content is being unloaded.
-          // It's currently pretty important to unload your static fields like this, to avoid having parts of your mod remain in memory when it's been unloaded.
-            LightbulbRing = null;
+            Timer++;
+
+            if (Timer % 60 == 0 && !(NumOfImages >= maxNumImages))
+            {
+                Projectile.damage += 1;
+                NumOfImages++;
+            }
+
+            if (player.statMana <= 0)
+            {
+                Projectile.Kill();
+            }
+
+            if (NumOfImages >= maxNumImages)
+            {
+                NumOfImages = maxNumImages;
+            }
+
+            if (!player.channel && channel == false)
+            {
+                channel = true;
+                Projectile.velocity = Projectile.DirectionTo(Main.MouseWorld) * 2 * NumOfImages;
+                
+                Projectile.timeLeft = 30 * (int)NumOfImages;
+            }
+
+            if(channel == false)
+            {
+                Projectile.Center = player.Center;
+            }
+            else
+            {
+                Projectile.velocity *= 0.98f;
+                if (Timer % 20 == 0)
+                {
+                    NumOfImages--;
+                }
+            }
+
+            Lighting.AddLight(Projectile.Center, Color.LightGoldenrodYellow.ToVector3() * 0.9f * NumOfImages / maxNumImages);
         }
         public override bool PreDraw(ref Color lightColor)
         {
-            Color drawColor = Lighting.GetColor((int)Projectile.Center.X / 16, (int)(Projectile.Center.Y / 16));
+            for (int i = 1; i <= NumOfImages; i++)
+            {
+                float scaleFloat = 0.65f - 0.25f / i * (float)Math.Sin(Timer / 20) / 2;
+                scale = new(scaleFloat, scaleFloat);
 
-            Main.EntitySpriteDraw(LightbulbRing.Value, Projectile.Center - Main.screenPosition,
-                          LightbulbRing.Value.Bounds, Color.NavajoWhite, Projectile.rotation,
-                          LightbulbRing.Size() * 0.5f, 1f, SpriteEffects.None, 0);
-            return true;
+                float colorMultiplier = 0.75f - scale.X;
+                Vector2 offset = new Vector2(tex.Width / 2, tex.Height / 2) * scale;
+
+                Main.EntitySpriteDraw(tex, Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY) - offset,
+                null, Color.LightGoldenrodYellow * colorMultiplier, Projectile.rotation, Projectile.Hitbox.Size() / 2, scale, SpriteEffects.None);
+            }
+            return false;
+        }
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            return Vector2.Distance(Projectile.Center, targetHitbox.Center.ToVector2()) <= tex.Width * scale.X / 2;
         }
     }
 }
-
