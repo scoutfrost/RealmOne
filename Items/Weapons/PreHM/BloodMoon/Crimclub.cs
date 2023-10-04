0b91d6c4e3439e53ea13b6daabd35307647ce17f
scoutfrost <125180429+scoutfrost@users.noreply.github.com>
2023-10-03 22:47:36 -0700

    crimclub code

--- Items/Weapons/PreHM/BloodMoon/Crimclub.cs
+++ Items/Weapons/PreHM/BloodMoon/Crimclub.cs
@@ -43,17 +43,92 @@ namespace RealmOne.Items.Weapons.PreHM.BloodMoon
         }
     }
 
-    public class CrimclubSwing : ModProjectile
+    public class CrimClubSwing : ModProjectile
     {
-        public override string Texture => "RealmOne/Items/Weapons/PreHM/BloodMoon/Crimclub";
-
-        public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
+        public override string Texture => "Items/Weapons/PreHM/BloodMoon/CrimclubSwing";
+        public override void SetDefaults()
         {
-            behindNPCsAndTiles.Add(index);
+            Projectile.width = 30;
+            Projectile.height = 30;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.timeLeft = 360;
+            Projectile.penetrate = -1;
+            Projectile.tileCollide = false;
+            Projectile.ownerHitCheck = true;
         }
+        int i = 0;
+        List<Vector2> keypoints = new();
+        Vector2 mouse;
+        Vector2 vectorToMouse;
+        Vector2 mousew;
+        ProjKeyFrameHandler keyFrameHandler;
+        public override void OnSpawn(IEntitySource source)
+        {
+            Player player = Main.player[Projectile.owner];
 
+            if (player.GetModPlayer<CrimClubPlayer>().SwingCount % 2 != 0)
+                upswing = true;
+            else
+                upswing = false;
 
+            keyFrameHandler = new(KeyFrameInterpolationCurve.Slerp, "RealmOne/Common/Core/HeldProjPoints/SwingPoints", 25);
+
+            mousew = Main.MouseWorld;
+            vectorToMouse = player.Center.DirectionTo(mousew);
+
+            keyFrameHandler.SetAiDefaults(Projectile, player, mousew);
+            keypoints = keyFrameHandler.GetPoints(40);
+
+            if ((player.direction == -1 && !upswing) || (player.direction == 1 && upswing))
+                i = keypoints.Count - 1;
+        }
+        bool upswing = false;
+        public override void AI()
+        {
+            Player player = Main.player[Projectile.owner];
+            mouse = player.Center + vectorToMouse;
+            keyFrameHandler.SetAiDefaults(Projectile, player, mouse);
+            Projectile.Center = keyFrameHandler.CalculateSwordSwingPointsAndApplyRotation(Projectile, mouse, player, keypoints, ref i, new Vector2(-5, -5), upswing);
+
+            player.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, Projectile.rotation + MathHelper.Pi + MathHelper.PiOver4 * player.direction);
+        }
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            if (Main.rand.NextBool(2))
+            {
+                Vector2 vel = new(Main.rand.NextFloat(-5, 5), -10);
+                Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, vel, ModContent.ProjectileType<BloodToothProj>(), Projectile.damage / 2, 0);
+            }
+        }
     }
-
-
+    public class CrimClubPlayer : ModPlayer
+    {
+        public int SwingCount = 0;
+    }
+    public class BloodToothProj : ModProjectile
+    {
+        public override string Texture => "Items/Weapons/PreHM/BloodMoon/BloodToothProj";
+        public override void SetDefaults()
+        {
+            Projectile.width = 10;
+            Projectile.height = 10;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.timeLeft = 360;
+            Projectile.penetrate = -1;
+            Projectile.tileCollide = true;
+        }
+        public override void AI()
+        {
+            Projectile.velocity += new Vector2(0, 0.5f);
+            if (Projectile.velocity.Y >= 16)
+            {
+                Projectile.velocity.Y = 16;
+            }
+            Projectile.rotation = Projectile.velocity.ToRotation();
+        }
+    }
 }
